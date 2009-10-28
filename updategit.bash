1f64a92421e4034683446c9b5a9c7255811284e0
Johan Sageryd <j416@1616.se>
2009-10-29 03:47:58 +0900

    Initial commit

---
+++ updategit.bash
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Downloads and installs specified version of Git to /usr/local/git-version,
+# and symlinks /usr/local/git -> /usr/local/git-version.
+#
+# Creates path and manpath in /etc/paths.d and /etc/manpaths.d/
+# Set $NEWGIT variable before run.
+# Run example:
+# $ NEWGIT=git-1.6.5 ./updategit.bash
+
+if [[ "$NEWGIT" != git* ]]; then
+	echo "\$NEWGIT var not set. Run example:"
+	echo "NEWGIT=git-1.6.5 ./updategit.bash"
+	exit 1
+fi
+
+# Download Git
+echo 'Downloading Git...'
+curl -O "http://www.kernel.org/pub/software/scm/git/$NEWGIT.tar.gz" || exit 1
+
+# Extract
+echo 'Extracting...'
+tar xvzf "$NEWGIT.tar.gz" || exit 1
+
+# Remove tar
+echo 'Removing tarball...'
+rm "$NEWGIT.tar.gz" || exit 1
+
+# Configure and make
+echo 'Running configure & make...'
+pushd "$NEWGIT"
+make configure || exit 1
+./configure --prefix=/usr/local/"$NEWGIT" || exit 1
+make || exit 1
+
+# Install
+echo 'Running make install...'
+sudo make install || exit 1
+
+# Remove source files
+echo 'Removing Git source files...'
+popd
+rm -rf "$NEWGIT" || exit 1
+
+# Update Git symlink
+echo 'Updating symlink /usr/local/git ...'
+sudo ln -sf /usr/local/"$NEWGIT" /usr/local/git || exit 1
+
+# Create /etc/paths.d/git
+echo 'Setting path in /etc/paths.d/git ...'
+echo '/usr/local/git/bin' | sudo tee /etc/paths.d/git || exit 1
+
+# Create /etc/manpaths.d/git
+echo 'Setting paths in /etc/manpaths.d/git ...'
+echo '/usr/local/git/share/man' | sudo tee /etc/manpaths.d/git || exit 1
+echo '/usr/local/gitmanpages' | sudo tee -a /etc/manpaths.d/git || exit 1
+
+# Done
+which -s git
+if [ $? -eq 0 ]; then
+	echo -n "Git updated: "
+	git --version
+else
+	echo "Git installed."
+fi
---
+++ updategitmanpages.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Downloads and installs specified version of Git manpages to /usr/local/gitmanpages.
+# WARNING NOTE: This script DELETES any previous content in '/usr/local/gitmanpages'.
+#
+# Set $NEWGITMANPAGES variable before run.
+# Run example:
+# $ NEWGITMANPAGES=git-manpages-1.6.5 ./updategitmanpages.bash
+
+if [[ "$NEWGITMANPAGES" != git* ]]; then
+	echo "\$NEWGITMANPAGES var not set. Run example:"
+	echo "NEWGITMANPAGES=git-manpages-1.6.5 ./updategitmanpages.bash"
+	exit 1
+fi
+
+# Remove old directory
+echo 'Removing /usr/local/gitmanpages ...'
+sudo rm -rf /usr/local/gitmanpages
+
+# Create new directory
+echo 'Creating new /usr/local/gitmanpages ...'
+sudo mkdir -p /usr/local/gitmanpages || exit 1
+pushd /usr/local/gitmanpages || exit 1
+
+# Download manpages
+echo 'Downloading Git man pages...'
+sudo curl -O "http://www.kernel.org/pub/software/scm/git/$NEWGITMANPAGES.tar.gz" || exit 1
+
+# Extract
+echo 'Extracting...'
+sudo tar xvzf "$NEWGITMANPAGES.tar.gz" || exit 1
+
+# Remove tar
+echo 'Removing tarball...'
+sudo rm "$NEWGITMANPAGES.tar.gz" || exit 1
+
+# Done
+popd
+echo "Git man pages updated."
